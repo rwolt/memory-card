827c5414ad833fa63e924a19483d3a740c8e5011
Raymond Wolt <rjwolt@gmail.com>
2021-10-11 17:09:28 -0500

    Refactor card image to use es6 imports

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ function Card(props) {
             className="card"
             onClick={props.handleClick}
             id={props.id}>
-            <img className="card-image" src={process.env.PUBLIC_URL + `/images/${props.image}`} alt={props.name} />
+            <img className="card-image" src={props.image} alt={props.name} />
             <p className="card-name">{props.name}</p>
         </div>
     )
---
+++ src/images/index.js
@@ -0,0 +1,59 @@
+import babyluigi from './babyluigi.png';
+import babymario from './babymario.png';
+import bowser from './bowser.png';
+import crankykong from './crankykong.png'
+import daisy from './daisy.png';
+import diddykong from './diddykong.png';
+import dixiekong from './dixiekong.png';
+import donkeykong from './donkeykong.png';
+import funkykong from './funkykong.png';
+import goomba from './goomba.png';
+import kameck from './kameck.png';
+import koopatroopa from './koopatroopa.png';
+import link from './link.png';
+import luigi from './luigi.png';
+import mario from './mario.png';
+import peach from './peach.png';
+import poochy from './poochy.png';
+import princesszelda from './princesszelda.png';
+import rambi from './rambi.png';
+import rosalina from './rosalina.png';
+import shyguy from './shyguy.png';
+import snowmads from './snowmads.png';
+import tikitaktribe from './tikitaktribe.png';
+import toad from './toad.png';
+import toadette from './toadette.png';
+import waluigi from './waluigi.png';
+import wario from './wario.png';
+import yoshi from './yoshi.png';
+
+export {
+    babyluigi,
+    babymario,
+    bowser,
+    crankykong,
+    daisy,
+    diddykong,
+    dixiekong,
+    donkeykong,
+    funkykong,
+    goomba,
+    kameck,
+    koopatroopa,
+    link,
+    luigi,
+    mario,
+    peach,
+    poochy,
+    princesszelda,
+    rambi,
+    rosalina,
+    shyguy,
+    snowmads,
+    tikitaktribe,
+    toad,
+    toadette,
+    waluigi,
+    wario,
+    yoshi
+}
